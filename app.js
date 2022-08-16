da7f8b3012729bbc0c77308c66594d3cc19a0270
amina-work <mina.works28@gmail.com>
2022-08-16 13:24:16 +0100

    progress checkpoint finished navbar, header, categories, courses, faqs sections html and scss and js desktop query only

---
+++ app.js
@@ -0,0 +1,23 @@
+// navbar change on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+
+//show & hide faq questions
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //change icon from + to -
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus'){
+            icon.className = "uil uil-minus";
+        } else{
+            icon.className = "uil uil-plus";
+        }
+    })
+})
